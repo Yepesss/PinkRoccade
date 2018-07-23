5007b27e728d1fee9583dd6d90738bef25c05c79
Jennes <jennesderonde@msn.com>
2018-07-23 17:51:16 +0200

    first commit

---
+++ src/main/java/com/pinkroccade/assignment/OpdrachtApplication.java
@@ -0,0 +1,12 @@
+package com.pinkroccade.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpdrachtApplication.class, args);
+	}
+}
